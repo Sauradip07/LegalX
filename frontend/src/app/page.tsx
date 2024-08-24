f269bd47fe50897be79b4a0d2ab2936045012e07
Abhishek Baranwal <baranwalabhishek21@gmail.com>
2024-08-25 01:07:48 +0530

    landing page pushed

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,207 +1,82 @@
 "use client";
-
-import { ChatLayout } from "@/components/chat/chat-layout";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogContent,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import UsernameForm from "@/components/username-form";
-import { getSelectedModel } from "@/lib/model-helper";
-import { ChatOllama } from "@langchain/community/chat_models/ollama";
-import { AIMessage, HumanMessage } from "@langchain/core/messages";
-import { BytesOutputParser } from "@langchain/core/output_parsers";
-import { ChatRequestOptions } from "ai";
-import { Message, useChat } from "ai/react";
-import React, { useEffect, useRef, useState } from "react";
-import { toast } from "sonner";
-import { v4 as uuidv4 } from "uuid";
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Home() {
-  const {
-    messages,
-    input,
-    handleInputChange,
-    handleSubmit,
-    isLoading,
-    error,
-    stop,
-    setMessages,
-    setInput,
-  } = useChat({
-    onResponse: (response) => {
-      if (response) {
-        setLoadingSubmit(false);
-      }
-    },
-    onError: (error) => {
-      setLoadingSubmit(false);
-      toast.error("An error occurred. Please try again.");
-    },
-  });
-  const [chatId, setChatId] = React.useState<string>("");
-  const [selectedModel, setSelectedModel] = React.useState<string>(
-    getSelectedModel()
-  );
-  const [open, setOpen] = React.useState(false);
-  const [ollama, setOllama] = useState<ChatOllama>();
-  const env = process.env.NODE_ENV;
-  const [loadingSubmit, setLoadingSubmit] = React.useState(false);
-  const formRef = useRef<HTMLFormElement>(null);
+  const [activeTab, setActiveTab] = useState('Our Idea');
 
-  useEffect(() => {
-    if (messages.length < 1) {
-      // Generate a random id for the chat
-      console.log("Generating chat id");
-      const id = uuidv4();
-      setChatId(id);
-    }
-  }, [messages]);
+  const tabs = ['Our Idea', 'Our Mission', 'Our Market'];
 
-  React.useEffect(() => {
-    if (!isLoading && !error && chatId && messages.length > 0) {
-      // Save messages to local storage
-      localStorage.setItem(`chat_${chatId}`, JSON.stringify(messages));
-      // Trigger the storage event to update the sidebar component
-      window.dispatchEvent(new Event("storage"));
-    }
-  }, [chatId, isLoading, error]);
-
-  useEffect(() => {
-    if (env === "production") {
-      const newOllama = new ChatOllama({
-        baseUrl: process.env.NEXT_PUBLIC_OLLAMA_URL || "http://localhost:11434",
-        model: selectedModel,
-      });
-      setOllama(newOllama);
-    }
-
-    if (!localStorage.getItem("ollama_user")) {
-      setOpen(true);
-    }
-  }, [selectedModel]);
-
-  const addMessage = (Message: any) => {
-    messages.push(Message);
-    window.dispatchEvent(new Event("storage"));
-    setMessages([...messages]);
-  };
-
-  // Function to handle chatting with Ollama in production (client side)
-  const handleSubmitProduction = async (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
-    e.preventDefault();
-
-    addMessage({ role: "user", content: input, id: chatId });
-    setInput("");
-
-    if (ollama) {
-      try {
-        const parser = new BytesOutputParser();
-
-        const stream = await ollama
-          .pipe(parser)
-          .stream(
-            (messages as Message[]).map((m) =>
-              m.role == "user"
-                ? new HumanMessage(m.content)
-                : new AIMessage(m.content)
-            )
-          );
-
-        const decoder = new TextDecoder();
-
-        let responseMessage = "";
-        for await (const chunk of stream) {
-          const decodedChunk = decoder.decode(chunk);
-          responseMessage += decodedChunk;
-          setLoadingSubmit(false);
-          setMessages([
-            ...messages,
-            { role: "assistant", content: responseMessage, id: chatId },
-          ]);
-        }
-        addMessage({ role: "assistant", content: responseMessage, id: chatId });
-        setMessages([...messages]);
-
-        localStorage.setItem(`chat_${chatId}`, JSON.stringify(messages));
-        // Trigger the storage event to update the sidebar component
-        window.dispatchEvent(new Event("storage"));
-      } catch (error) {
-        toast.error("An error occurred. Please try again.");
-        setLoadingSubmit(false);
-      }
-    }
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setLoadingSubmit(true);
-
-    setMessages([...messages]);
-
-    // Prepare the options object with additional body data, to pass the model.
-    const requestOptions: ChatRequestOptions = {
-      options: {
-        body: {
-          selectedModel: selectedModel,
-        },
-      },
-    };
-
-    if (env === "production") {
-      handleSubmitProduction(e);
-    } else {
-      // Call the handleSubmit function with the options
-      handleSubmit(e, requestOptions);
-    }
-  };
-
-  const onOpenChange = (isOpen: boolean) => { 
-    const username = localStorage.getItem("ollama_user")
-    if (username) return setOpen(isOpen)
-
-    localStorage.setItem("ollama_user", "Anonymous")
-    window.dispatchEvent(new Event("storage"))
-    setOpen(isOpen)
-  }
-  
   return (
-    <main className="flex h-[calc(100dvh)] flex-col items-center ">
-      <Dialog open={open} onOpenChange={onOpenChange}>
-        <ChatLayout
-          chatId=""
-          setSelectedModel={setSelectedModel}
-          messages={messages}
-          input={input}
-          handleInputChange={handleInputChange}
-          handleSubmit={onSubmit}
-          isLoading={isLoading}
-          loadingSubmit={loadingSubmit}
-          error={error}
-          stop={stop}
-          navCollapsedSize={10}
-          defaultLayout={[30, 160]}
-          formRef={formRef}
-          setMessages={setMessages}
-          setInput={setInput}
-        />
-        <DialogContent className="flex flex-col space-y-4">
-          <DialogHeader className="space-y-2">
-            <DialogTitle>Welcome to Ollama!</DialogTitle>
-            <DialogDescription>
-              Enter your name to get started. This is just to personalize your
-              experience.
-            </DialogDescription>
-            <UsernameForm setOpen={setOpen} />
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+    <main>
+      <div>
+        <div className="relative">
+          <Image
+            src="/landing.jpg"
+            alt="Background"
+            objectFit="cover"
+            width={1920}
+            height={1080}
+            sizes="100vw 100vh"
+            quality={100}
+          />
+            <div className="absolute bottom-0 text-center font-title  h-[48%] w-full">
+          <div className="inset-0 flex flex-col items-center justify-center z-10 p-8 text-white">
+              <h1 className="text-6xl  z-[9] font-bold mb-2">LegalX: Simplifying Law for All</h1>
+              <p className="text-3xl text-black  z-[9]">Demystifying Legal Jargon for All</p>
+              <div className="absolute inset-0 bottom-0 h-full w-full bg-gradient-to-t from-white to-transparent blur-lg pointer-events-none"></div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-8">
+          <div className="flex font-title justify-center space-x-4 mb-4">
+            {tabs.map((tab) => (
+              <button
+                key={tab}
+                className={`px-4 py-2 ${
+                  activeTab === tab
+                    ? 'text-blue-500 border-b-2 border-blue-500'
+                    : 'text-gray-500'
+                }`}
+                onClick={() => setActiveTab(tab)}
+              >
+                {tab}
+              </button>
+            ))}
+          </div>
+          <div className="border-t border-gray-300 p-5 px-[20%]">
+            {activeTab === 'Our Idea' && (
+              <div>
+                <h2 className="text-center text-lg font-semibold mb-4 font-title">What we do ?</h2>
+                <p className='text-center text-2xl font-title'>LegalX is a product that simplifies complex legal jargon and privacy policies, making them accessible and understandable for everyone, ensuring users can navigate legal documents with confidence.</p>
+              </div>
+            )}
+            {activeTab === 'Our Mission' && (
+              <div>
+                <h2 className="text-center text-lg font-semibold mb-4">Why we exist</h2>
+                <p className="mb-4 text-center text-2xl font-title">
+                  Many individuals and small businesses struggle with understanding legal terms, leading to
+                  potential misunderstandings and mistakes. LegalX breaks down these barriers, offering clear
+                  and concise interpretations accessible to all.
+                </p>
+              </div>
+            )}
+            {activeTab === 'Our Market' && (
+              <div className=''>
+                <h2 className="text-center text-lg font-semibold mb-4 font-title">Who we help</h2>
+                <p className='text-center text-2xl font-title'>
+                  LegalX targets the legal technology (LegalTech) market, specifically focusing on the consumer
+                  legal services segment. It also appeals to the privacy and compliance sector, offering value to
+                  individuals, small businesses, and organizations that require clear, accessible legal information.
+                  Additionally, it intersects with the digital privacy and educational technology markets, catering to
+                  those who need simplified explanations of complex legal jargons in documents.
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -72,6 +72,10 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        title: ['Merriweather','serif'],
+        subtitle: [ "Baskervville SC", 'serif']
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
